865a2cff1d2e4273919a96b4d13062c41b426aa1
Amritraj mishra <officialamrit06825@gmail.com>
2025-06-06 17:47:27 +0530

    this side first commited

---
+++ index.js
@@ -0,0 +1,20 @@
+function addValue(val) {
+  document.getElementById("display").value += val;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+function backspace(){
+  let display = document.getElementById("display");
+  display.value =display.value.slice(0,-1);
+
+}
+function calculate() {
+  try {
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch {
+    document.getElementById("display").value = "Error";
+  }
+}
